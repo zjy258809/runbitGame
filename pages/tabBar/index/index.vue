e7213c168ca0478cd7e9be476f46a1e155e96f56
vivilau <1484473667@qq.com>
2022-08-22 19:54:45 +0800

    添加获取装备，装备卡槽，更新步数等方法

--- pages/tabBar/index/index.vue
+++ pages/tabBar/index/index.vue
@@ -16,7 +16,7 @@
 			</view>
 
 			<view class="person">
-				<img class="user_person" src="../../../static/group5.png"></img>
+				<img class="user_person" src="../../../static/Group5.png"></img>
 				<img  @tap="inputDialogToggle()" class="userlogo" src="../../../static/group-5-2.png"></img>
 				<view class="user_person_item uni-flex uni-column">
 				<img class="gp11 " src="../../../static/chooseImg.png" />
@@ -70,7 +70,7 @@
 
 			<img @tap="getDay()" class="gp4" src="../../../static/Group4.png" />
 
-			<view class="bs">已同步1000步</view>
+			<view class="bs">已同步{{steps}}步</view>
 
 		</view>
 
@@ -91,7 +91,7 @@
 
 			</uni-popup>
 		</view>
-		
+
 		<!-- 装备选择 -->
 		<view>
 			<uni-popup ref="inputDialog4" type="dialog">
@@ -122,102 +122,138 @@
 </template>
 
 <script>
-	import {
-		ethers,
-		BigNumber
-	} from 'ethers'
-	import {
-		refStoreAddress,
-		refAbi,
-		RunbitCollectionAddress,
-		RunbitCollectionAbi,
-		RBAddress,
-		RBAbi
-	} from '../../../contract/address.js'
-	import {
-		useContract,
-		getApproveState,
-		contractApprove
-	} from '../../../contract/useContract.js'
-	export default {
-		data() {
-			return {
-				value: '',
-				type: 'center',
-				scrrenHeight: 10,
-				value: "",
-				title: 'Hello',
-				layerAddress: '',
-				rpcaddr: '',
-				myAccount: '',
-				layerAbi: '',
-				collectContract: null,
-				approveState: true
-			}
-		},
-		onLoad() {
-			//      this.rpcaddr = getApp().globalData.rpcaddr;
-			// this.layerAddress = getApp().globalData.layerAddress;
-			//  this.layerAbi = getApp().globalData.layerAbi;
-		},
-		mounted() {
-			this.scrrenHeight = uni.getSystemInfoSync().windowHeight;
-			// this.$refs.isopen.open();
-			try {
+import {
+	ethers,
+	BigNumber
+} from 'ethers'
+import {
+	refStoreAddress,
+	refAbi,
+	RunbitCollectionAddress,
+	RunbitCollectionAbi,
+	RBAddress,
+	RBAbi, RunbitAddress,
+	RunbitAbi
+} from '../../../contract/address.js'
+import {
+	useContract,
+	getApproveState,
+	contractApprove
+} from '../../../contract/useContract.js'
+import { setTrack, getTrackId, getBindEquips, getBindCards } from '../../../contract/useRunbit.js'
+export default {
+	data() {
+		return {
+			value: '',
+			type: 'center',
+			scrrenHeight: 10,
+			value: "",
+			title: 'Hello',
+			layerAddress: '',
+			rpcaddr: '',
+			myAccount: '',
+			layerAbi: '',
+			collectContract: null,
+			approveState: true,
+			steps: 0
+		}
+	},
+	onLoad() {
+		//      this.rpcaddr = getApp().globalData.rpcaddr;
+		// this.layerAddress = getApp().globalData.layerAddress;
+		//  this.layerAbi = getApp().globalData.layerAbi;
+	},
+	mounted() {
+		this.scrrenHeight = uni.getSystemInfoSync().windowHeight;
+		// this.$refs.isopen.open();
+		try {
 
-				const provider = new ethers.providers.Web3Provider(window.ethereum);
-				provider.send("eth_requestAccounts", []).then(accounts => {
-					this.myAccount = accounts[0]
-					//判断是否需要填激活码		 
-					useContract(refStoreAddress, refAbi).then(refContract => {
-						refContract.referrer(this.myAccount).then(refer => {
-							if (refer === '0x0000000000000000000000000000000000000000')
-								this.$refs.isopen.open();
-						})
-					})						 
-
-				});
-
-
-
-			} catch (e) {
-				console.error(e);
-			}
-
-		},
-		beforeCreate() {
-
-		},
-		methods: {
-
-			async approve() {
-
-			},
-			inputDialogToggle() {
-				this.$refs.inputDialog.open()
-			},
-			dialogInputConfirm(val) {
-				uni.showLoading({
-					title: '3秒后会关闭'
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			provider.send("eth_requestAccounts", []).then(accounts => {
+				this.myAccount = accounts[0]
+				//判断是否需要填激活码		 
+				useContract(refStoreAddress, refAbi).then(refContract => {
+					refContract.referrer(this.myAccount).then(refer => {
+						if (refer === '0x0000000000000000000000000000000000000000')
+							this.$refs.isopen.open();
+					})
 				})
+				useContract(RunbitAddress, RunbitAbi).then(runContract => {
+					this.runContract = runContract
+					//获取赛道
+					getTrackId(this.runContract).then(id => {
+						this.trackId = id.toNumber()
+						console.log("trackid ", this.trackId)
+					})
+					//获取装备,0代表没装备
+					// this.equips = getBindEquips(this.runContract, this.myAccount)
+					console.log("equips ", this.equips)
+
+					//更新步数
+					// this.updateStep()
+				})
+			});
+			//测试
+			setTimeout(() => {
+				//点击装备获取卡槽情况
+				// var equipType =0 //上衣
+				// getBindCards(this.runContract, this.equips[equipType].id).then(cards => {
+				// 	this.cards = cards
+				// })
+			}, 3000)
 
 
-				this.$refs.inputDialog.close()
-			},
-			async dialogInputConfirm2(val) {
-				console.log(val)
-				try {
-					let tx = await this.contract.addReferrerWithCheck(val)
-					console.log(tx.hash)
-					uni.showLoading({
-						title: '请稍等...'
-					})
-					tx.wait().then(res => {
-						uni.hideLoading()
-					})
-				} catch (e) {
-					console.error(e)
-				}
+		} catch (e) {
+			console.error(e);
+		}
+
+	},
+	beforeCreate() {
+
+	},
+	methods: {
+
+		async approve() {
+
+		},
+		inputDialogToggle() {
+			this.$refs.inputDialog.open()
+		},
+		async dialogInputConfirm() {
+			uni.showLoading({
+				title: '更新赛道中...'
+
+			})
+			this.$refs.inputDialog.close()
+			console.log("---------track------", this.setTrackId)
+			try {
+				let tx = await setTrack(this.runContract, this.setTrackId)
+				await tx.wait()
+			} catch (e) {
+				console.error(e)
+				//todo 错误提示
+			} finally {
+				uni.hideLoading()
+			}
+
+
+
+
+		},
+		async dialogInputConfirm2(val) {
+			console.log(val)
+			try {
+				let tx = await this.contract.addReferrerWithCheck(val)
+				console.log(tx.hash)
+				uni.showLoading({
+					title: '请稍等...'
+				})
+				tx.wait().then(res => {
+					uni.hideLoading()
+				})
+			} catch (e) {
+				console.error(e)
+			}
 
 				// setTimeout(() => {
 				// 	uni.hideLoading()
@@ -279,8 +315,8 @@
 
 //
 
-		}
 	}
+}
 </script>
 
 <style>
@@ -322,147 +358,148 @@
 		justify-content: center;
 		height: 100%;
 
-	}
+}
 
-	.bg {
-		position: absolute;
-		width: 100%;
-		height: 26.875rem;
-		left: 0px;
-		top: 0px;
-		background: linear-gradient(180deg, #FFF7B0 0%, rgba(255, 247, 176, 0) 100%);
-		border-radius: 0px 0px 36px 36px;
-	}
+.bg {
+	position: absolute;
+	width: 100%;
+	height: 26.875rem;
+	left: 0px;
+	top: 0px;
+	background: linear-gradient(180deg, #FFF7B0 0%, rgba(255, 247, 176, 0) 100%);
+	border-radius: 0px 0px 36px 36px;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
 
-	}
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+	display: flex;
+	justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
 
-	/* 修改 */
-	.currentbs {
-		display: flex;
+/* 修改 */
+.currentbs {
+	display: flex;
 
-		flex-direction: row;
-		font-weight: bold;
-		align-items: center;
-		width: 6.25rem;
-	}
+	flex-direction: row;
+	font-weight: bold;
+	align-items: center;
+	width: 6.25rem;
+}
 
-	.currentImg {
-		width: 0.625rem;
-		height: 0.625rem;
-		display: block;
-		margin: auto 0;
-		margin-left: 0.825rem;
-	}
+.currentImg {
+	width: 0.625rem;
+	height: 0.625rem;
+	display: block;
+	margin: auto 0;
+	margin-left: 0.825rem;
+}
 
-	.currentImg2 {
-		width: 1.5rem;
-		height: 1.5rem;
-		display: block;
-		margin: auto 0;
-		margin-left: 0.825rem;
-	}
+.currentImg2 {
+	width: 1.5rem;
+	height: 1.5rem;
+	display: block;
+	margin: auto 0;
+	margin-left: 0.825rem;
+}
 
-	.userName {
-		display: block;
-		margin: auto 0;
-		margin-left: 1.1rem;
+.userName {
+	display: block;
+	margin: auto 0;
+	margin-left: 1.1rem;
 		width: 10rem;
-		height: 2.25rem;
-	}
+	height: 2.25rem;
+}
 
-	.person {
-		width: 30%;
-		height: 21rem;
-		margin: 0 auto;
-	}
+.person {
+	width: 30%;
+	height: 21rem;
+	margin: 0 auto;
+}
 
-	.gp11 {
+.gp11 {
 		margin-top: 5.2rem;
 		margin-left: 0.625rem;
 		width: 200.88rpx;
-	}
+}
 
-	.gp12 {
+.gp12 {
 		width: 200.88rpx;
 		margin-left: 0.825rem;
-	}
+}
 
-	/* //修改 */
-	.gp5 {
-		width: 50%;
-		margin: 0 auto;
-		display: inline-block;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+/* //修改 */
+.gp5 {
+	width: 50%;
+	margin: 0 auto;
+	display: inline-block;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-	}
+}
 
-	.curId {
-		width: 13.25rem;
-		margin: 0.225rem;
-	}
+.curId {
+	width: 13.25rem;
+	margin: 0.225rem;
+}
 
-	.idvalue {
-		margin-left: 9rem;
-	}
+.idvalue {
+	margin-left: 9rem;
+}
 
-	.idvalue2 {
-		margin-left: 7rem;
-	}
+.idvalue2 {
+	margin-left: 7rem;
+}
 
-	.gp4 {
-		display: inline-block;
-		margin: 1.25rem auto;
-		width: 30%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+.gp4 {
+	display: inline-block;
+	margin: 1.25rem auto;
+	width: 30%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-	}
+}
 
-	.bs {
-		width: 100%;
-		display: inline-block;
-		text-decoration: underline;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		font-size: 0.85rem;
-		margin: 0 auto;
-		margin-top: -0.5rem;
-		height: 69.44rpx;
-		margin-bottom: 3rem;
-		text-align: center;
+.bs {
+	width: 100%;
+	display: inline-block;
+	text-decoration: underline;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	font-size: 0.85rem;
+	margin: 0 auto;
+	margin-top: -0.5rem;
+	height: 69.44rpx;
+	margin-bottom: 3rem;
+	text-align: center;
 
-	}
+}
 
-	.dialogimg {
-		display: block;
-		margin: 0 auto;
-		width: 408.33rpx;
-		height: 248.88rpx;
-		padding: 6.94rpx;
+.dialogimg {
+	display: block;
+	margin: 0 auto;
+	width: 408.33rpx;
+	height: 248.88rpx;
+	padding: 6.94rpx;
 
-	}
+}
 </style>
+>>>>>>> Stashed changes
